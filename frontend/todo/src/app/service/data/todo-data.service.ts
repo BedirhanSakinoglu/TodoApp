83361dffb48b8ece8c84def4604b1a20258ee9a9
BedirhanSakinoglu <bedirhansakinoglu@gmail.com>
2022-02-14 14:26:42 +0300

    Security added

--- frontend/todo/src/app/service/data/todo-data.service.ts
+++ frontend/todo/src/app/service/data/todo-data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Todo } from 'src/app/list-todos/list-todos.component';
 
@@ -30,4 +30,5 @@ export class TodoDataService {
   createTodo(username:string, todo:Todo){
     return this.http.post(`http://localhost:8080/users/${username}/todos`, todo);
   }
+
 }
